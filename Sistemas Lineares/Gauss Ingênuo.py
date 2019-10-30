8a3be7d611516fb858bc587b7534615fef865219
mickaelyoshua <mickael_yoshua@hotmail.com>
2019-10-30 11:23:17 -0300

    Update Gauss Ingênuo.py

--- Sistemas Lineares/Gauss Ingênuo.py
+++ Sistemas Lineares/Gauss Ingênuo.py
@@ -1,27 +1,23 @@
 import numpy as np
 
-def trian_sup(M):
+def trian_sup(M):                        #Foi feita uma função a parte para deixar a matriz na forma de triângulo superior
   for j in range(M.shape[0]-1):
     for i in range(M.shape[0]-1):
       if i==j:
-        pivo=M[i,j]
-      mul=M[i+1,j]/pivo
-      M[i+1,:]=M[i+1,:]-M[j,:]*mul
+        pivo=M[i,j]                      #Definição do pivô
+      mul=M[i+1,j]/pivo                  #Cálculo do multiplicador
+      M[i+1,:]=M[i+1,:]-M[j,:]*mul       #Subtração e substituição das linhas zerando a coluna abaixo do pivô
   return M
 
-def gauss_ingenuo(M):
-  x=np.zeros(M.shape[0])
-  M=trian_sup(M)
-  for j in range(M.shape[0]-1, -1, -1):
+def gauss_ingenuo(A):
+  M=A.copy()                             #Tirada a cópia da matriz
+  x=np.zeros(M.shape[0])                 #Matriz das raizes nulas
+  M=trian_sup(M)                         #Retorna uma triângulo superior
+  for j in range(M.shape[0]-1, -1, -1):  #Obtenção das raízes através da fórmula apresentada no método de Gauss Seidel
     independente=M[j,-1]
     soma=0.0
     for i in range(M.shape[0]):
       if j!=i:
         soma+=M[j,i]*x[i]
     x[j]=(independente-soma)/M[j,j]
-    
-  return x
-
-A=np.array([[1,2,1,12],[1,-3,5,1],[2,-1,3,10]], dtype=float)
-
-print(gauss_ingenuo(A))
+   
