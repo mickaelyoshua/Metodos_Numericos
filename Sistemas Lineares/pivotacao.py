d9802d97dca74756fbb00fdd14828635bd3fbe99
mickaelyoshua <mickael_yoshua@hotmail.com>
2019-10-30 11:27:13 -0300

    Update and rename Pivotação.py to pivotacao.py

--- Sistemas Lineares/pivotacao.py
+++ Sistemas Lineares/pivotacao.py
@@ -5,7 +5,3 @@ def pivotacao(M):                # Função para prática de pivotação
     indice=np.argmax(M[i:,i])+i
     M[[indice,i]]=M[[i,indice]]
   return M
-
-A=np.array([[1,2,1,12],[1,-3,5,1],[2,-1,3,10]], dtype=float)
-
-print(pivotacao(A))
